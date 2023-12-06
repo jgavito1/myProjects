4df56349942008209bffdcb6365c879b941a6f19
Justin Gavito <justingavito1994@gmail.com>
2023-12-05 23:13:07 -0600

    Added a new class and new functions for a dropdown menu

--- new-app.py
+++ new-app.py
@@ -1,9 +1,29 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.menu import MDDropdownMenu
+
+class Start(MDBoxLayout):
+    def drpmenu(self,instance):
+            self.menu_list = [
+                { 
+                    'viewclass': 'OneLineListItem',
+                    'text': 'Example 1',
+                    'on_release': lambda x : self.addbtn()
+                }
+            ]
+            self.menu = MDDropdownMenu(items=self.menu_list,width_mult=2)
+            self.menu.caller = instance
+            self.menu.open()
+
+    def addbtn(self):
+        pass
 
 class NewApp(MDApp):
     def build(self):
-        self.title = "Idk what this app is supposed to be right now"
-        return Builder.load_file('new-app.kv')
+        Builder.load_file('new-app.kv')
+        return Start()
+
+        
 
 NewApp().run()

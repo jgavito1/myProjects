d32fec0fb2107d3ae1ab2f7d84c3c5b42d9ceb1c
Justin Gavito <justingavito1994@gmail.com>
2023-12-06 22:59:40 -0600

    Replaced dropdown menu with Navigation Drawer

--- new-app.py
+++ new-app.py
@@ -1,29 +1,16 @@
-from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.menu import MDDropdownMenu
+from kivymd.app import MDApp
 
-class Start(MDBoxLayout):
-    def drpmenu(self,instance):
-            self.menu_list = [
-                { 
-                    'viewclass': 'OneLineListItem',
-                    'text': 'Example 1',
-                    'on_release': lambda x : self.addbtn()
-                }
-            ]
-            self.menu = MDDropdownMenu(items=self.menu_list,width_mult=2)
-            self.menu.caller = instance
-            self.menu.open()
 
-    def addbtn(self):
-        pass
+
+class ContentNavigationDrawer(MDBoxLayout):
+    pass
 
 class NewApp(MDApp):
     def build(self):
-        Builder.load_file('new-app.kv')
-        return Start()
+        self.theme_cls.theme_style = "Light"
+        return Builder.load_file('new-app.kv')
 
-        
 
 NewApp().run()

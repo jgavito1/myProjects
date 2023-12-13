97334cd51d3bc77ab223122565e28b4fbdf92a52
Justin Gavito <justingavito1994@gmail.com>
2023-12-12 19:53:04 -0600

    added a list of items for a dropdown menu to select your character level

--- DndApp.py
+++ DndApp.py
@@ -11,7 +11,7 @@ class ContentNavigationDrawer(MDScrollView):
     screen_manager = ObjectProperty()
     nav_drawer = ObjectProperty()
 # Class defining the main functionality of the app
-class DND(MDApp):    
+class DND(MDApp):
     def build(self):
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'Orange'
@@ -19,40 +19,56 @@ class DND(MDApp):
         self.screen = Builder.load_file('DndApp.kv')
         items = [
             'Artificer', 'Barbarian', 'Bard', 'Blood Hunter', 'Cleric', 'Druid', 'Fighter',
-            'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard' 
+            'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard', 
         ]
         menu_items = [
             {
                 "viewclass": "OneLineListItem",
                 "text": f"{item}",
                 "on_release": lambda x=f"{item}": self.class_select(x)
-            } for item in items
-             
+            } for item in items 
+        ]
+        level_items = [
+            {
+                "viewclass": "OneLineListItem",
+                "text": f"{i}",
+                "on_release": lambda x=f"{i}": self.level_select(x)
+            } for i in range(1, 20+1)
         ]
         self.menu = MDDropdownMenu(
             caller=self.screen.ids.class_id, # type: ignore
             items=menu_items,
             width_mult=3,
-            max_height= '224dp',
+            max_height= '300dp'
+        )
+        self.level = MDDropdownMenu(
+            caller=self.screen.ids.level_id, # type: ignore
+            items=level_items,
+            width_mult=3,
+            max_height= '300dp'
         )
         return self.screen
-    
-    # Updates class_name textfield when menu item is selected
+
+
+    # Updates class button and level button text when menu item is selected
     def class_select(self, text_item):
-        self.root.ids.class_name.text = text_item # type: ignore
+        self.root.ids.class_id.text = text_item # type: ignore
         self.menu.dismiss()
-    
+    def level_select(self, text_item):
+        self.root.ids.level_id.text = "LEVEL" + " " + text_item # type: ignore
+        self.level.dismiss()
+
      # Logic for the dice rolls for each dice if more than one
     def roll_dice(self):
         try:
             num_dice = int(self.root.ids.num_dice.text)# type: ignore
             num_sides = int(self.root.ids.num_sides.text)# type: ignore
             modifier = int(self.root.ids.modifier.text) if self.root.ids.modifier.text else 0 # type: ignore
-            
+
             if num_dice <= 0:
                 self.root.ids.result_output.text = "Invalid input"# type: ignore
                 return
-            
+
             rolls = [random.randint(1, num_sides) for dice in range(num_dice)]
             total = sum(rolls) + modifier
             result_text = f"Rolls: {rolls}\nTotal with modifier ({modifier}): {total}"

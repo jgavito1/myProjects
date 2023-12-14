232216320fa8e1efd65032791c9f9c46aec12c88
Justin Gavito <justingavito1994@gmail.com>
2023-12-13 23:07:14 -0600

    Added to DnD DiceRoller/Character Sheet app Contains lists that were moved from DndApp.py

---
+++ menu_items.py
@@ -0,0 +1,10 @@
+# This file will store any list for the character sheet
+class_item = [
+    'Artificer', 'Barbarian', 'Bard', 'Blood Hunter', 'Cleric', 'Druid', 'Fighter',
+    'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard', 
+]
+race = [
+    'Dwarf', 'Dragonborn', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Half-Orc', 'Human', 'Tielfing' 
+]
+
+

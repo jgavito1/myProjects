c9bb2652a02797d0c6a2d3bf61359e975b9fc0ca
Justin Gavito <justingavito1994@gmail.com>
2023-12-18 00:44:34 -0600

    updated menu_items.py list to include subraces

--- menu_items.py
+++ menu_items.py
@@ -1,10 +1,19 @@
-# This file will store any list for the character sheet
+#This python file will keep track of lists for character sheet.
+#Not everything will be used
 class_item = [
-    'Artificer', 'Barbarian', 'Bard', 'Blood Hunter', 'Cleric', 'Druid', 'Fighter',
-    'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard', 
+    'ARTIFICER', 'BARBARIAN', 'BARD', 'BLOOD HUNTER', 'CLERIC', 'DRUID', 'FIGHTER',
+    'MONK', 'PALADIN', 'RANGER', 'ROGUE', 'SORCERER', 'WARLOCK', 'WIZARD', 
 ]
 race = [
-    'Dwarf', 'Dragonborn', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Half-Orc', 'Human', 'Tielfing' 
+    'DWARF', 'DRAGONBORN', 'ELF', 'GNOME', 'HALF-ELF', 'HALFLING', 'HALF-ORC', 'HUMAN', 'TIEFLING' 
 ]
 
+all_subraces = [
+            'HILL DWARF', 'MOUNTAIN DWARF', 'HIGH ELF', 'WOOD ELF', 'DARK ELF', 'LIGHTFOOT HALFLING','STOUT HALFLING',
+            'FOREST GNOME', 'ROCK GNOME',    
+        ]
 
+dwarf_subclass = ['HILL DWARF'',', 'MOUNTAIN DWARF']
+elf_subclass = ['HIGH ELF'',', 'WOOD ELF'',', 'DARK ELF']
+halfling_subclass = ['LIGHTFOOT HALFLING'',','STOUT HALFLING']
+gnome_subclass = ['FOREST GNOME'',', 'ROCK GNOME']

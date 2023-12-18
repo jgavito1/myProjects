99d8b799f79826e9e7cf8a987c9d3a418fafc5fb
Justin Gavito <justingavito1994@gmail.com>
2023-12-18 00:11:06 -0600

    updated the subrace_select function. Still incomplete.

--- DndApp.py
+++ DndApp.py
@@ -4,9 +4,8 @@ from kivymd.app import MDApp
 from kivymd.uix.scrollview import MDScrollView
 import random
 from kivymd.uix.menu import MDDropdownMenu
-from menu_items import class_item
-from menu_items import race
-from menu_items import all_subraces
+from menu_items import class_item, race, dwarf_subclass, elf_subclass, halfling_subclass, gnome_subclass, all_subraces
+
 
 # Adds content inside of the Navigation Drawer for the user to click on
 class ContentNavigationDrawer(MDScrollView):
@@ -25,6 +24,12 @@ class DND(MDApp):
                 "on_release": lambda x=f"{item}": self.class_select(x)
             } for item in class_item 
         ]
+        self.menu = MDDropdownMenu(
+            caller=self.screen.ids.class_id, # type: ignore
+            items=menu_items,
+            width_mult=3,
+            max_height= '300dp'
+        )
         level_items = [
             {
                 "viewclass": "OneLineListItem",
@@ -32,6 +37,12 @@ class DND(MDApp):
                 "on_release": lambda x=f"{i}": self.level_select(x)
             } for i in range(1, 20+1)
         ]
+        self.level = MDDropdownMenu(
+            caller=self.screen.ids.level_id, # type: ignore
+            items=level_items,
+            width_mult=3,
+            max_height= '300dp'
+        )
         race_items = [
             {
                 "viewclass": "OneLineListItem",
@@ -39,6 +50,12 @@ class DND(MDApp):
                 "on_release": lambda x=f"{item}": self.race_select(x)
             } for item in race
         ]
+        self.race = MDDropdownMenu(
+            caller=self.screen.ids.race_id, # type: ignore
+            items=race_items,
+            width_mult=3,
+            max_height= '300dp'
+        )
         subrace_items = [
             {
                 "viewclass": "OneLineListItem",
@@ -46,24 +63,6 @@ class DND(MDApp):
                 "on_release": lambda x=f"{item}": self.subrace_select(x)
             } for item in all_subraces
         ]
-        self.menu = MDDropdownMenu(
-            caller=self.screen.ids.class_id, # type: ignore
-            items=menu_items,
-            width_mult=3,
-            max_height= '300dp'
-        )
-        self.level = MDDropdownMenu(
-            caller=self.screen.ids.level_id, # type: ignore
-            items=level_items,
-            width_mult=3,
-            max_height= '300dp'
-        )
-        self.race = MDDropdownMenu(
-            caller=self.screen.ids.race_id, # type: ignore
-            items=race_items,
-            width_mult=3,
-            max_height= '300dp'
-        )
         self.subrace = MDDropdownMenu(
             caller=self.screen.ids.subrace_id, # type: ignore
             items=subrace_items,
@@ -84,7 +83,16 @@ class DND(MDApp):
         self.root.ids.race_id.text = text_item # type: ignore
         self.race.dismiss()
     def subrace_select(self, text_item):
-        self.root.ids.subrace_id.text = text_item
+        if self.root.ids.race_id.text == 'DWARF': # type: ignore
+            self.root.ids.subrace_id.text = 'HILL DWARF' # type: ignore
+            if text_item == 'HILL DWARF':
+                self.root.ids.subrace_id.text = 'HILL DWARF' # type: ignore
+            elif text_item == 'MOUNTAIN DWARF':
+                self.root.ids.subrace_id.text = 'MOUNTAIN DWARF' # type: ignore
+            else:
+                self.root.ids.subrace_id.text = 'NOT VALID SUBCLASS' # type: ignore
+        else:
+            self.root.ids.subrace_id.text = text_item # type: ignore
         self.subrace.dismiss()
     
      # Logic for the dice rolls for each dice if more than one
